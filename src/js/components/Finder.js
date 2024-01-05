fff673d5fcf2ffa7f401fd9632b6e3126e239fff
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2024-01-05 16:07:38 +0100

    Add table with hover effect, header and confirmation button

--- src/js/components/Finder.js
+++ src/js/components/Finder.js
@@ -6,13 +6,13 @@ class Finder {
     const thisFinder = this;
 
     thisFinder.render(element);
-    // AOS.init();
+    AOS.init();
   }
 
   render(element) {
     const thisFinder = this;
 
-    const generatedHTML = templates.finderPage();
+    let generatedHTML = templates.finderPage();
 
     thisFinder.dom = {};
 
@@ -21,8 +21,48 @@ class Finder {
     /* Create element using utils.createElementFromHTML */
     thisFinder.element = utils.createDOMFromHTML(generatedHTML);
 
+    let generalInnerHTML = '';
+
+    for (let i = 0; i < thisFinder.element.children.length; i++) {
+
+      let child = thisFinder.element.children[i];
+
+      if (child.classList.contains('matrix')){
+        const button = thisFinder.element.querySelector('.square');
+        const row = thisFinder.element.querySelector('.row');
+        const matrix = thisFinder.element.querySelector('.matrix');
+        let buttons = '';
+        let rows = '';
+        let letters = 'abcdefghij';
+    
+        for(let i = 0; i < letters.length; i++){
+    
+          row.setAttribute('data-id', letters[i]);
+    
+          for(let j = 0; j < 10; j++){
+            button.setAttribute('data-id', 1 + j);
+            buttons += button.outerHTML;
+          }
+    
+          row.innerHTML = buttons;
+          buttons = '';
+          rows += row.outerHTML;
+        }
+    
+        matrix.innerHTML = rows;
+        generalInnerHTML += matrix.outerHTML;
+
+      } else if (child == thisFinder.element.firstElementChild) {
+        generalInnerHTML = child.outerHTML;
+      } else {
+        generalInnerHTML += child.outerHTML;
+      }
+    }
+
+    thisFinder.element.innerHTML = generalInnerHTML;
+
     /* Add element to #menu */
-    thisFinder.dom.wrapper.innerHTML = generatedHTML;
+    thisFinder.dom.wrapper.innerHTML = thisFinder.element.outerHTML;
   }
 }
 
